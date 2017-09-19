61c2d767a72fcd58e0bf25670174236c0d5ac0f2
Just <JustLee.me@hotmail.com>
2017-09-19 21:41:14 +0800

    Initial Commit

---
+++ RealmDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RealmDemo
+//
+//  Created by Just on 2017/9/19.
+//  Copyright © 2017年 Just. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RealmDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RealmDemo
+//
+//  Created by Just on 2017/9/19.
+//  Copyright © 2017年 Just. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
